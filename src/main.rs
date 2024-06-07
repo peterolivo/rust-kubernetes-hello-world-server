4e599da27bfebaa33b7e92a2ec94c72cbe8c6386
Peter Olivo <7140818+peterolivo@users.noreply.github.com>
2024-06-07 10:34:43 -0700

    rewrite: add cargo, fib and frog

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,97 @@
-use std::io::Write;
-use std::net::TcpListener;
+#![allow(unreachable_code)]
+use rouille::router;
+
+fn fib() -> u128 {
+    let mut a: u64 = 0;
+    let mut b = 1;
+    for l in 0..10_000_000 {
+        println!("{}", l);
+        let c = std::hint::black_box(a + b);
+        a = std::hint::black_box(b);
+        b = std::hint::black_box(c);
+    }
+    let d = 1;
+    return d;
+}
+
+fn get_frog() -> &'static str {
+    r#"
+                               .-----.
+                              /7  .  (
+                             /   .-.  \
+                            /   /   \  \
+                           / `  )   (   )
+                          / `   )   ).  \
+                        .'  _.   \_/  . |
+       .--.           .' _.' )`.        |
+      (    `---...._.'   `---.'_)    ..  \
+       \            `----....___    `. \  |
+        `.           _ ----- _   `._  )/  |
+          `.       /"  \   /"  \`.  `._   |
+            `.    ((O)` ) ((O)` ) `.   `._\
+              `-- '`---'   `---' )  `.    `-.
+                 /                  ` \      `-.
+               .'                      `.       `.
+              /                     `  ` `.       `-.
+       .--.   \ ===._____.======. `    `   `. .___.--`     .''''.
+      ' .` `-. `.                )`. `   ` ` \          .' . '  8)
+     (8  .  ` `-.`.               ( .  ` `  .`\      .'  '    ' /
+      \  `. `    `-.               ) ` .   ` ` \  .'   ' .  '  /
+       \ ` `.  ` . \`.    .--.     |  ` ) `   .``/   '  // .  /
+        `.  ``. .   \ \   .-- `.  (  ` /_   ` . / ' .  '/   .'
+          `. ` \  `  \ \  '-.   `-'  .'  `-.  `   .  .'/  .'
+            \ `.`.  ` \ \    ) /`._.`       `.  ` .  .'  /
+      PRO    |  `.`. . \ \  (.'               `.   .'  .'
+          __/  .. \ \ ` ) \                     \.' .. \__
+   .-._.-'     '"  ) .-'   `.                   (  '"     `-._.--.
+  (_________.-====' / .' /\_)`--..__________..-- `====-. _________)
+                   (.'(.'`
+"#
+}
 
 fn main() {
-    println!("Starting!");
-    let listener = TcpListener::bind("0.0.0.0:8000").unwrap();
+    println!("Now listening on localhost:8000");
 
-    // The listener's accept method waits or 'blocks' until
-    // we have a connection and then returns a new TcpStream
-    // that we can read and write data to.
-    let message = "Hello, World!";
-    let response = format!(
-        "HTTP/1.1 200 OK\r\n\
-        Content-Type: text/html; charset=utf-8\r\n\
-        Content-Length: {}\r\n\
-        \r\n\
-        {}",
-        message.len(),
-        message
-    );
-    for stream in listener.incoming() {
-        println!("Stream recieved!");
-        let mut stream = stream.unwrap();
+    rouille::start_server("localhost:8000", move |request| {
+        router!(request,
+            (GET) (/) => {
+                rouille::Response::redirect_302("/hello/world")
+            },
 
-        stream.write_all(response.as_bytes()).unwrap();
-        println!("Response sent!");
-    }
+            (GET) (/frog) => {
+                rouille::Response::text(get_frog())
+            },
+
+            (GET) (/hello/world) => {
+                println!("hello world");
+
+                rouille::Response::text("hello world")
+            },
+
+            (GET) (/panic) => {
+                panic!("Oops!")
+            },
+
+            (GET) (/fib) => {
+                fib();
+
+                rouille::Response::text(format!("complete fib calc"))
+            },
+
+            (GET) (/{id: u32}) => {
+                println!("u32 {:?}", id);
+
+                rouille::Response::empty_400()
+            },
+
+            (GET) (/{id: String}) => {
+                println!("String {:?}", id);
+
+                rouille::Response::text(format!("hello, {}", id))
+            },
+
+            _ => rouille::Response::empty_404()
+            )
+        }
+    )
 }
